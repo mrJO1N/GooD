e797a7814d1adb4c8c6f31b07836999c77d887da
mrJO1N <zhenduk.zhenduk123456780@gmail.com>
2023-03-17 22:45:25 +0300

    new file:   GAMeG8D.js 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ GAMeG8D.js
@@ -0,0 +1,79 @@
+/*
+this code is test my skills
+
+and check "i can make this?"
+
+
+*/
+
+let colorTime, 
+  drawningTime,
+  ctx,
+  settings = {};
+
+function c( text ) {
+  console.log( text );
+};
+
+function setSettings(settingsTime) { // func for completion basic settings
+  let basic = { // basic settings for "settings"
+    "path": "body",
+    "height": 150,
+    "width": 300
+  },
+  basicKeys = Object.keys( basic ), // better make this now, than make this in cycle "for"
+  basicValues = Object.values( basic ),
+  reportTime = [];
+  
+  if ( typeof settingsTime != "undefined" ) {
+    for ( let i=0; i<basicKeys.length; i++ ) {   
+      if ( typeof settingsTime[ basicKeys[ i ] ] == "undefined" ) {
+        settings[ basicKeys[ i ] ] = basicValues[ i ]; // if no such value is specified
+        reportTime = reportTime + basicKeys[ i ];
+      } else {
+        settings[ basicKeys[ i ] ] = settingsTime[ basicKeys[ i ] ]; // if such value is specified
+      };
+    };
+    if ( reportTime.length != 0 ) console.log( "you don't write this in setSettings: " + reportTime ); // report warnings to console
+  } else {
+    settings = basic;
+  };
+};
+
+
+const Render = { // all graphics functions and methods
+
+  init: (canvStyle) => { 
+    let doc = document.querySelector( settings.path ),
+    canv = document.createElement( "canvas" );
+    ctx = canv.getContext( "2d" ); // work only in 2d space
+
+    doc.appendChild( canv ); 
+    
+    document.body.style = "margin: 0; padding: 0; overflow: hidden; ";
+    
+    canv.height = settings.height;
+    canv.width = settings.width;
+
+    canv.style = canvStyle;
+    
+  },
+
+  draw: ( content ) => {
+    ctx.clearRect( 0, 0, window.innerWidth, window.innerHeight );
+    for (const i of content) {
+      drawningTime = i.split( " " );
+
+      if ( drawningTime[0] != colorTime ) { //if the color is changed in the array (content), then the color being drawn changes
+        colorTime = drawningTime[0];
+        ctx.fillStyle = colorTime;
+      };
+
+      // ["color type x y width height"]
+      ctx.fillRect( drawningTime[2], drawningTime[3], drawningTime[4], drawningTime[5] ); // drawning square
+
+    }
+  },
+
+  clear: () => ctx.clearRect( 0, 0, settings.width, settings.height )
+}
---
+++ script.js
@@ -0,0 +1,75 @@
+setSettings( {
+  "height": window.innerHeight,
+  "width": window.innerWidth
+} );
+
+let drawningObjects = [ //["color type x y width height"]
+  "yellow square 0 0 300 200",
+  "black square 0 100 100 100",
+  "black square 200 100 100 100",
+  "black square 100 0 100 100"
+];
+
+// (width, height, css)
+Render.init( "border: solid 4px yellow; background-color: aqua" );
+
+Render.draw( drawningObjects );
+
+
+
+
+let Xtime = 0,
+  Ytime = 0,
+  Xtime2 = true,
+  Ytime2 = true,
+  forever = setInterval( () => {
+    if ( Xtime2 ) Xtime += 10; else Xtime -= 10;
+
+    if ( ( Xtime < 0 ) || ( Xtime > window.innerWidth-100 ) ) {
+      ( Xtime2 ) ? Xtime2 = false : Xtime2 = true;
+    }
+    
+
+    if ( Ytime2 ) Ytime += 10; else Ytime -= 10;
+
+    if ( ( Ytime < 0 ) || ( Ytime > window.innerHeight-100 ) ) {
+      ( Ytime2 ) ? Ytime2 = false : Ytime2 = true;
+    }
+
+console.log( Xtime, Ytime,window.innerWidth, window.innerHeight )
+
+    Render.draw( Array( `green square ${ Xtime } ${ Ytime } 100 100` ) );
+  },
+  10 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // ttime , 
+//   // ttime2 = ;
+
+// document.querySelector( "canvas" ).onmouseover = () => {
+//   none();
+// }
+// document.querySelector( "canvas" ).touchstart = () => none();
+
+// document.addEventListener( TouchEvent, none() );
+// document.addEventListener( "mouseover", none() );
+
