83d86c46bd44915a06eb50e3b4211911104c07f0
mrJO1N <zhenduk.zhenduk123456780@gmail.com>
2023-03-19 00:42:21 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ function start () {
     "width": window.innerWidth
   } );
 
-let drawningObjects = [ //["color type x y width height"]
-  "yellow square 0 0 300 200",
-  "black square 0 100 100 100",
-  "black square 200 100 100 100",
-  "black square 100 0 100 100"
-];
+// let drawningObjects = [ //["color type x y width height"]
+//   "yellow square 0 0 300 200",
+//   "black square 0 100 100 100",
+//   "black square 200 100 100 100",
+//   "black square 100 0 100 100"
+// ];
 
 // (width, height, css)
 Render.init( "border: solid 4px yellow; background-color: aqua" );
 
-Render.draw( drawningObjects );
+// Render.draw( drawningObjects );
 
 
-Render.draw( Array( "green circle 100 100 100" ) );
+// Render.draw( Array( "green circle 100 100 100" ) );
 
 
 let Xtime = 0,
@@ -40,7 +40,7 @@ let Xtime = 0,
 
     //console.log( Xtime, Ytime,window.innerWidth, window.innerHeight );
 
-    Render.draw( Array( `red square ${ Xtime } ${ Ytime } 100` ) );
+    Render.draw( Array( `red circle ${ Xtime } ${ Ytime } 100` ) );
   }, 
 10 );
 }
